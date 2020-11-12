705cffdb7727197c81a1b368128b5ca6a936a589
baodoan24hdev <baodoan.24hdev@gmail.com>
2020-11-12 09:06:01 +0700

    12/11/2020

--- resources/custom-jquery.js
+++ resources/custom-jquery.js
@@ -39,35 +39,53 @@ $(function(){
     //     }
     // });
 
-    $(".btn-submit").click(function(event){
+    $("#btn-submit").click(function(event){
+        $(".error").remove();
         event.preventDefault();
         let name = $("#name").val();
         let email = $("#email").val();
         let password = $("#password").val();
-        console.log(name);
-        if (name == undefined){
-            $("#name").after(<div class="error">Bạn chưa nhập tên đăng nhập!</div> );
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (name === ""){
+            $("#name").after("<div class='error'>Bạn chưa nhập tên đăng nhập!</div>");
             return false;
         }
-        return false;
-        console.log(name);
-        console.log(password);
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:3001/api/login",
-            data: {
-                username: name,
-                password: password
-            },
-            dataType: "json",
-            success: function(da){
-                alert(da.message);
-            },
-            error: function(da){
-                console.log(da);
-                alert(da.responseJSON.error)
-            }
-        })
+        else if (email === ""){
+            $("#email").after("<div class='error'>Bạn chưa nhập email!</div>");
+            return false;
+        }
+        else if (re.test(email) === false){
+            $("#email").after("<div class='error'>Email của bạn không hợp lệ!</div>");
+            return false;
+        }
+        else if (password === ""){
+            $("#password").after("<div class='error'>Bạn chưa nhập mật khẩu!</div>");
+            return false;
+        }
+        else if (password.length < 8){
+            $("#password").after("<div class='error'>Độ dài mật khẩu lớn hơn 8!</div>");
+            return false;
+        }
+        else{
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:3001/api/login",
+                data: {
+                    username: name,
+                    password: password
+                },
+                dataType: "json",
+                success: function(da){
+                    alert(da.message);
+                    $("#btn-submit").prop("disabled", true);
+                    $("#btn-submit").css("opacity", "0.6");
+                },
+                error: function(da){
+                    console.log(da);
+                    alert(da.responseJSON.error)
+                }
+            })
+        }
     })
     
 
